c15738596e7110cd9d5a716fb30bbdd7a7ba1765
Khoa Tran <autumns.sun@gmail.com>
2016-08-04 11:49:19 +1000

    Initial Commit

---
+++ workbench/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  workbench
+//
+//  Created by admin on 4/08/2016.
+//  Copyright © 2016 stethee.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
