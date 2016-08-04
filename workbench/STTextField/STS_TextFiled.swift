a31741abc1f9139d25259d88713acfe724f40186
Khoa Tran <autumns.sun@gmail.com>
2016-08-04 20:31:47 +1000

    add git ignore

--- workbench/STTextField/STS_TextFiled.swift
+++ workbench/STTextField/STS_TextFiled.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 //@IBDesignable public class STTextFiled:UITextField{
-@IBDesignable public class STTextFiled:UITextField{
+@IBDesignable public class STS_TextFiled:UITextField{
     
     var TitileLabel:UILabel! //= UILabel()
     var ErrorLabel:UILabel! //= UILabel()
